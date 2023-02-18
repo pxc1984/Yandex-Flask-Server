2e8ccf1032575069b7348252fc0a9e97103cf398
JDtrimble <igor.mamaev1@gmail.com>
2023-02-18 18:17:21 +0300

    Beginning of work on the tasks

--- server.py
+++ server.py
@@ -2,12 +2,52 @@ from flask import Flask, url_for, request, render_template, redirect
 import json
 from flask_wtf import FlaskForm
 from wtforms import  StringField, PasswordField, BooleanField, SubmitField, EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 # Большинство из этих импортов могут быть не нужны именно в этой задаче, 
 # но для удобства они все равно добавлены
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kLxEZ8<_q[5$%N#Z'
+
+EDUCATION_TYPES = [
+    "Никакое",
+    "none",
+    "Базовое",
+    "Основное",
+    "Специальное",
+    "Высшее"
+]
+
+GENDERS = [
+    "Муж", "Мужчина", "Парень", "м", "male",
+    "Жен", "Женщина", "Девушка", "ж", "female",
+    "не определился"
+]
+
+
+def education_validation(form, field):
+    if field.data not in EDUCATION_TYPES:
+        raise ValidationError("Такого образования нет")
+
+def gender_validator(form, field):
+    if field.data not in GENDERS:
+        raise ValidationError("На борьбу с мутантами мутантов не берут.")
+
+class MissionForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    education = StringField('Образование', validators=[DataRequired(), education_validation])
+    profession = StringField('Профессия', validators=[])
+    gender = StringField('Пол', validators=[DataRequired(), gender_validator])
+    motivation = StringField('Опишите, почему вы хотите попасть к нам', validators=[DataRequired()])
+    allowance = BooleanField('Готовы остаться на марсе?', validators=[DataRequired(message="Без вашего согласия вас не получится запихнуть на вечно на марс. МУХАХАХАХАХАХХАХ")])
+    submit = SubmitField('Записаться')
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('base.html', title="Home")
 
 
 @app.route('/index/<title>')
@@ -25,6 +65,14 @@ def training(prof):
     else:
         param['prof'] = False
     return render_template('trainer.html', **param)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = MissionForm()
+    if form.validate_on_submit():
+        return redirect('/home')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/list_prof/<list>')
